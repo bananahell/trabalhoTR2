cc74ac65362b3d41eeb2fdc7295170e60d83bbd4
Lucca Huguet <luccahuguet@hotmail.com>
2022-08-14 22:42:34 -0300

    refactoring & prettier UI

--- code/client.py
+++ code/client.py
@@ -1,7 +1,7 @@
-from inspect import isclass
+# from inspect import isclass
 import socket
 import threading
-from utils import *
+import utils as utils
 
 
 client = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
@@ -10,22 +10,30 @@ connected = True
 
 def main():
     print("[STARTING] Client is starting...")
-    client.connect(ADDR)
-    username = input('Input your username: ')
-    room = input('Choose a room to join: ')
-    print(f"\nRoom {room}\n")
-    message = username.encode(FORMAT)
+
+    client.connect(utils.ADDR)
+
+    global username
+    global room
+    username = input("Input your username: ")
+    room = input("Choose a room to join: ")
+
+    message = username.encode(utils.FORMAT)
     usernameLength = len(message)
-    sendLength = str(usernameLength).encode(FORMAT)
-    sendLength += b' ' * (HEADER-len(sendLength))
+    sendLength = str(usernameLength).encode(utils.FORMAT)
+    sendLength += b" " * (utils.HEADER - len(sendLength))
+
     client.send(sendLength)
     client.send(message)
-    message = room.encode(FORMAT)
+
+    message = room.encode(utils.FORMAT)
     roomLength = len(message)
-    sendLength = str(roomLength).encode(FORMAT)
-    sendLength += b' ' * (HEADER-len(sendLength))
+    sendLength = str(roomLength).encode(utils.FORMAT)
+    sendLength += b" " * (utils.HEADER - len(sendLength))
+
     client.send(sendLength)
     client.send(message)
+
     listener = threading.Thread(target=listen)
     sender = threading.Thread(target=send)
     listener.start()
@@ -35,22 +43,26 @@ def main():
 def listen():
     global connected
     while connected:
-        print(client.recv(2048).decode(FORMAT))
+        print(client.recv(2048).decode(utils.FORMAT).strip())
 
 
 def send():
     global connected
     while connected:
         msg = input()
-        if (msg == DISCONNECT_MESSAGE):
+        if msg == utils.DISCONNECT_MESSAGE:
             connected = False
-        message = msg.encode(FORMAT)
+        message = msg.encode(utils.FORMAT)
         msgLength = len(message)
-        sendLength = str(msgLength).encode(FORMAT)
-        sendLength += b' ' * (HEADER-len(sendLength))
+        sendLength = str(msgLength).encode(utils.FORMAT)
+        sendLength += b" " * (utils.HEADER - len(sendLength))
         client.send(sendLength)
         client.send(message)
+        if connected:
+            print("\033[A", end="")  # move the cursor up a line
+            print("\033[K", end="")  # erase the line
+            print(f"[Room {room}][{username}]: " + msg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
